eeea3577c2b7fd45a7d3a9a79a53b1c9c0ba253c
Arif <arifmajeed007.as@gmail.com>
2023-07-04 14:10:51 +0530

    SchemeBot network error fix 4

--- packages/mylearning/src/pages/SchemeBot.js
+++ packages/mylearning/src/pages/SchemeBot.js
@@ -204,7 +204,7 @@ const SchemeBot = () => {
         const queryString = `You are a helpful assistant who helps with answering questions based on the provided information. If the information asked cannot be found in the text provided, you admit that I can't find the exact information. Always include application links for each scheme. Here is the question: ${responseData}`;
         console.log(queryString);
         const secondApiResponse = await axios.get(
-          "https://4.240.112.55:8000/query-with-gptindex",
+          "http://4.240.112.55:8000/query-with-gptindex",
 
           {
             params: {
